1b7864d5af288cb09d9701cc50d30f3aa265ee0d
nameless <79296308+wty-xjtu@users.noreply.github.com>
2023-01-16 12:33:02 +0800

    Add files via upload

--- CODE/classify.py
+++ CODE/classify.py
@@ -18,7 +18,6 @@ def data_split(src_data_folder, target_data_folder, train_scale=0.8, val_scale=0
         return
 
     print("开始数据集划分")
-    class_names = os.listdir(src_data_folder)
     # 在目标目录下创建文件夹
     split_names = ['train', 'val', 'test']
     for split_name in split_names:
@@ -29,39 +28,37 @@ def data_split(src_data_folder, target_data_folder, train_scale=0.8, val_scale=0
             os.mkdir(split_path)
 
     # 按照比例划分数据集，并进行数据的复制
-    for class_name in class_names:
-        current_class_data_path = src_data_folder
-        current_all_data = os.listdir(current_class_data_path)
-        current_data_length = len(current_all_data)
-        current_data_index_list = list(range(current_data_length))
-        random.shuffle(current_data_index_list)
+    current_all_data = os.listdir(src_data_folder)
+    current_data_length = len(current_all_data)
+    current_data_index_list = list(range(current_data_length))
+    random.shuffle(current_data_index_list)
 
-        train_folder = os.path.join(target_data_folder, 'train')
-        val_folder = os.path.join(target_data_folder, 'val')
-        test_folder = os.path.join(target_data_folder, 'test')
-        train_stop_flag = current_data_length * train_scale
-        val_stop_flag = current_data_length * (train_scale + val_scale)
-        current_idx = 0
-        train_num = 0
-        val_num = 0
-        test_num = 0
-        for i in current_data_index_list:
-            src_img_path = os.path.join(current_class_data_path, current_all_data[i])
-            if current_idx <= train_stop_flag:
-                copy2(src_img_path, train_folder)
-                # print("{}复制到了{}".format(src_img_path, train_folder))
-                train_num = train_num + 1
-            elif (current_idx > train_stop_flag) and (current_idx <= val_stop_flag):
-                copy2(src_img_path, val_folder)
-                # print("{}复制到了{}".format(src_img_path, val_folder))
-                val_num = val_num + 1
-            else:
-                copy2(src_img_path, test_folder)
-                # print("{}复制到了{}".format(src_img_path, test_folder))
-                test_num = test_num + 1
-            current_idx = current_idx + 1
+    train_folder = os.path.join(target_data_folder, 'train')
+    val_folder = os.path.join(target_data_folder, 'val')
+    test_folder = os.path.join(target_data_folder, 'test')
+    train_stop_flag = current_data_length * train_scale
+    val_stop_flag = current_data_length * (train_scale + val_scale)
+    current_idx = 0
+    train_num = 0
+    val_num = 0
+    test_num = 0
+    for i in current_data_index_list:
+        src_img_path = os.path.join(src_data_folder, current_all_data[i])
+        if current_idx <= train_stop_flag:
+            copy2(src_img_path, train_folder)
+            # print("{}复制到了{}".format(src_img_path, train_folder))
+            train_num = train_num + 1
+        elif (current_idx > train_stop_flag) and (current_idx <= val_stop_flag):
+            copy2(src_img_path, val_folder)
+            # print("{}复制到了{}".format(src_img_path, val_folder))
+            val_num = val_num + 1
+        else:
+            copy2(src_img_path, test_folder)
+            # print("{}复制到了{}".format(src_img_path, test_folder))
+            test_num = test_num + 1
+        current_idx = current_idx + 1
 
-    print("数据集{}按照{}：{}：{}的比例划分完成，一共{}个文件".format(class_name, train_scale, val_scale, test_scale, current_data_length))
+    print("数据集{}按照{}：{}：{}的比例划分完成，一共{}个文件".format(src_data_folder, train_scale, val_scale, test_scale, current_data_length))
     print("训练集{}：{}个文件".format(train_folder, train_num))
     print("验证集{}：{}个文件".format(val_folder, val_num))
     print("测试集{}：{}个文件".format(test_folder, test_num))
